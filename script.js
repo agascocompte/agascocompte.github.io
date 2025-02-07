41983c129d95c5ac12069d6afe1ec3d34db5710d
Arturo <a.gascocompte@gmail.com>
2025-02-07 23:10:55 +0100

    added header, footer and icon

---
+++ script.js
@@ -0,0 +1,10 @@
+const nav = document.querySelector("nav");
+
+window.addEventListener("scroll", function () {
+    if (window.scrollY > 50) { 
+        nav.classList.add("scrolled");
+    } else {
+        nav.classList.remove("scrolled");
+    }
+});
+
